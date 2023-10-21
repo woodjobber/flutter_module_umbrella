db59e3ea337ac4d2c73aee10616010edf7cf764f
soer <474436731@qq.com>
2023-10-21 16:37:14 +0800

    StreamController<bool> completer = StreamController<bool>();

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,146 @@
-import 'dart:io';
+import 'dart:async';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_module_login/flutter_module_login.dart';
 import 'package:flutter_module_register/flutter_module_register.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(UmbrellaApp(
-    route: WidgetsBinding.instance.platformDispatcher.defaultRouteName,
-  ));
-}
+StreamSubscription<bool>? subscription;
 
-class UmbrellaApp extends StatelessWidget {
-  const UmbrellaApp({
-    super.key,
-    required this.route,
+void main() async {
+  const chanel = MethodChannel("io.flutter.update.entrypoint");
+  CustomFlutterBinding();
+  await initialize();
+
+  chanel.setMethodCallHandler((call) async {
+    return await runAppForRoute(call);
   });
-  final String route;
-  @override
-  Widget build(BuildContext context) {
-    stdout.writeln("++++++ $route");
-    return switch (route) {
-      UmbrellaModules.login => const LoginPage(),
-      UmbrellaModules.register => const RegisterPage(),
-      _ => const ErrorApp(),
-    };
-  }
+  final route = WidgetsBinding.instance.platformDispatcher.defaultRouteName;
+
+  runApp(widgetForRoute(route));
 }
 
-class ErrorApp extends StatelessWidget {
-  const ErrorApp({super.key});
+Future runAppForRoute(MethodCall call) async {
+  final route = call.arguments.toString();
+  debugPrint(route);
+  if (call.method == "destroy.flutter.app") {
+    subscription?.cancel();
+    runApp(widgetForRoute(route));
+    return Future.value(null);
+  }
+
+  /// 防止 手速过快 导致 页面混乱 【快闪】 无法一次性进入目标页面
+  StreamController<bool> completer = StreamController<bool>();
+  subscription = completer.stream.listen((event) {
+    runApp(widgetForRoute(route));
+  });
+  runApp(SplashPage(
+    completer: completer,
+    key: const ValueKey(100),
+  ));
+  return Future.value(null);
+}
+
+Widget widgetForRoute(String route) {
+  return switch (route) {
+    UmbrellaModules.login => const LoginPage(
+        key: ValueKey(1),
+      ),
+    UmbrellaModules.register => const RegisterPage(
+        key: ValueKey(2),
+      ),
+    UmbrellaModules.splash => const CommandPage(
+        key: ValueKey(3),
+      ),
+    _ => const EmptyApp(
+        key: ValueKey(4),
+      ),
+  };
+}
+
+class EmptyApp extends StatelessWidget {
+  const EmptyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: const Text('error'),
+        body: Material(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SplashPage extends StatelessWidget {
+  final StreamController<bool> completer;
+
+  const SplashPage({required this.completer, Key? key}) : super(key: key);
+  final bool animated = false;
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      completer.sink.add(true);
+    });
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Material(
+          child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            child: Stack(
+              alignment: Alignment.center,
+              fit: StackFit.expand,
+              children: [
+                const Align(
+                  alignment: Alignment.center,
+                  child: FlutterLogo(
+                    size: 100,
+                  ),
+                ),
+                const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.orangeAccent,
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    child: animated
+                        ? SizedBox(
+                            width: 250.0,
+                            child: TextLiquidFill(
+                              text: '北京欢迎您！',
+                              waveColor: Colors.orangeAccent,
+                              boxBackgroundColor: Colors.deepPurple,
+                              textStyle: const TextStyle(
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueAccent),
+                              boxHeight: 100.0,
+                            ),
+                          )
+                        : const Text(
+                            '北京欢迎您！',
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.orangeAccent),
+                          ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -47,4 +150,5 @@ class ErrorApp extends StatelessWidget {
 class UmbrellaModules {
   static const login = 'login_module';
   static const register = 'register_module';
+  static const splash = 'splash';
 }
